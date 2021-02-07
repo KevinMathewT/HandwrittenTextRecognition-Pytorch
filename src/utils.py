316cac22da71ec502735d7b67a086868453db5d5
KevinMathewT <mathewtkevin@gmail.com>
2021-02-07 23:37:38 +0530

    edit for kaggle

--- src/utils.py
+++ src/utils.py
@@ -134,7 +134,7 @@ class EditDistanceMeter:
 
         total_distance = 0.0
 
-        for i in len(y_true):
+        for i in range(len(y_true)):
             pred = y_pred[i].view(config.TIME_STEPS, config.N_CLASSES)
             pred = torch.argmax(pred, 1)
             s = "".join([config.ID2CHAR[id.item()] for id in pred])
