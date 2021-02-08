3922a30cbb8b312a808ab93c30a9bf03c8afa5a3
KevinMathewT <mathewtkevin@gmail.com>
2021-02-09 01:15:51 +0530

    edit for kaggle

--- src/transforms.py
+++ src/transforms.py
@@ -283,9 +283,20 @@ def get_valid_transforms():
     return Compose([
         Rescale((config.W, config.H)),
         TransposeImage(),
+        # VerticalFlip(p=1),
+        # HorizontalFlip(p=0.5),
+        # ShiftScaleRotate(p=1., rotate_limit=5.),
+        # HueSaturationValue(hue_shift_limit=0.2,
+        #                    sat_shift_limit=0.2, val_shift_limit=0.2, p=0.5),
+        # RandomBrightnessContrast(
+        #     brightness_limit=(-0.1, 0.1), contrast_limit=(-0.1, 0.1), p=0.5),
+        # Rotate(p=0.5, border_mode=cv2.BORDER_CONSTANT, value=[255., 255., 255.], limit=5.),
         Normalize(mean=[0.485, 0.456, 0.406], std=[
                   0.229, 0.224, 0.225], max_pixel_value=255.0, p=1.0),
-        ToGrayscale(),
-        GreyscaleToBlackAndWhite(),
+        # Cutout(p=0.5),
+        # ToGrayscale(),
+
+        # # # GaussianFiltering(channels=1, kernel_size=5, sigma=1),
+        # GreyscaleToBlackAndWhite(),
         ToTensorV2(p=1.0),
     ], p=1.)
