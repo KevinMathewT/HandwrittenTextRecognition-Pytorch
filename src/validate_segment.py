e2c815f60b8d5c3d3174da55370f7f9f2f35debd
KevinMathewT <mathewtkevin@gmail.com>
2021-02-27 21:12:32 +0530

    added segment validation

--- src/validate_segment.py
+++ src/validate_segment.py
@@ -39,11 +39,7 @@ class HandWritingFormsDataset(Dataset):
             for i, line in enumerate(lines):
                 lines[i] = self.transforms(image=line)['image']
 
-        for i, line in enumerate(lines):
-            print(f"{i}: {line.shape}")
-        lines = np.ndarray(lines)
-        print(lines.shape)
-        lines = torch.tensor(lines)
+        lines = torch.stack(lines)
         print(lines.size())
 
         return lines, target
