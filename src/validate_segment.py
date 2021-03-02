9291fd575653ec4e865e7df453c33823eaf34877
KevinMathewT <mathewtkevin@gmail.com>
2021-03-02 11:11:50 +0530

    added form cropping using xml info

--- src/validate_segment.py
+++ src/validate_segment.py
@@ -78,7 +78,7 @@ def create_df():
     # forms = glob.glob(config.FORMS_PATH + "\*.png")  # PC
     df = pd.DataFrame(np.array(forms).reshape(-1, 1), columns=["path"])
     df["image_id"] = df.apply(
-        lambda row: row.path.split("\\")[-1].split('.')[0], axis=1)
+        lambda row: row.path.split("/")[-1].split('.')[0], axis=1)
     df["xml"] = df.apply(lambda row: os.path.join(
         config.GENERATED_FILES_PATH, "xml") + "/" + row.image_id + ".xml", axis=1)
     df["label"] = df.apply(lambda row: _parse_xml_file(row.xml), axis=1)
