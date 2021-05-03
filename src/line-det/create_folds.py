7e588814be5d6d5b5db80ce2d391d4df066dcccd
KevinMathewT <mathewtkevin@gmail.com>
2021-05-03 20:48:46 +0530

    added detr line detection

--- src/line-det/create_folds.py
+++ src/line-det/create_folds.py
@@ -92,8 +92,11 @@ def create_df():
         df["path"] = df.apply(lambda row: row.replace())
         df["image_id"] = df.apply(
             lambda row: row.path.split("\\")[-1].split('.')[0], axis=1)
-        df["xml"] = df.apply(lambda row: os.path.join(
-            config.GENERATED_FILES_PATH, "xml") + "/" + row.image_id + ".xml", axis=1)
+        df["xml"] = df.apply(lambda row:
+                             # os.path.join(config.GENERATED_FILES_PATH, "xml") +
+                             "/" +
+                             row.image_id +
+                             ".xml", axis=1)
         df["label"] = df.apply(lambda row: _parse_xml_file(row.xml), axis=1)
         # df[["x1", "x2", "y1", "y2"]] = df.apply(lambda row: _get_bb_of_item(row.xml), axis=1)
 
