fb7c945d145e247593c6d75f2575f6468a98d785
KevinMathewT <mathewtkevin@gmail.com>
2021-05-07 15:14:30 +0530

    edit for kaggle

--- src/line-det/detr/util/box_ops.py
+++ src/line-det/detr/util/box_ops.py
@@ -48,22 +48,19 @@ def generalized_box_iou(boxes1, boxes2):
     """
     # degenerate boxes gives inf / nan results
     # so do an early check
-    try:
-        assert (boxes1[:, 2:] >= boxes1[:, :2]).all()
-        assert (boxes2[:, 2:] >= boxes2[:, :2]).all()
-        iou, union = box_iou(boxes1, boxes2)
+    print("boxes1: ", boxes1)
+    print("boxes2: ", boxes2)
+    assert (boxes1[:, 2:] >= boxes1[:, :2]).all()
+    assert (boxes2[:, 2:] >= boxes2[:, :2]).all()
+    iou, union = box_iou(boxes1, boxes2)
 
-        lt = torch.min(boxes1[:, None, :2], boxes2[:, :2])
-        rb = torch.max(boxes1[:, None, 2:], boxes2[:, 2:])
+    lt = torch.min(boxes1[:, None, :2], boxes2[:, :2])
+    rb = torch.max(boxes1[:, None, 2:], boxes2[:, 2:])
 
-        wh = (rb - lt).clamp(min=0)  # [N,M,2]
-        area = wh[:, :, 0] * wh[:, :, 1]
+    wh = (rb - lt).clamp(min=0)  # [N,M,2]
+    area = wh[:, :, 0] * wh[:, :, 1]
 
-        return iou - (area - union) / area
-
-    except:
-        print("boxes1: ", boxes1)
-        print("boxes2: ", boxes2)
+    return iou - (area - union) / area
 
 
 def masks_to_boxes(masks):
